216fb1baeebc11893bb96108ae07bd0e6192e3ab
filipe_apolinario <f.apolinario30@gmail.com>
2015-10-13 10:32:08 +0100

    deleted print of stdout

--- server/server.js
+++ server/server.js
@@ -69,7 +69,7 @@ function serverHandler(request, response) {
 		var exec = require('child_process').exec, child;
 
 		child = exec('scanimage --format tiff'/* >/media/serverhdd/Users/'+name+'.tiff'*/, function (error, stdout, stderr) {
-			console.log('stdout: ' + stdout);
+			//console.log('stdout: ' + stdout);
 			console.log('stderr: ' + stderr);
 			if (error !== null) {
 			  console.log('exec error: ' + error);
